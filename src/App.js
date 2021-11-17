1606bf72ccd78f28089f6685e174a33a019efe86
90539 <yavuz.imer133@gmail.com>
2021-11-18 01:30:24 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Content() {
+    return (
+        <div>
+            aaaa
+        </div>
+    )
+}
+
+export default Content
