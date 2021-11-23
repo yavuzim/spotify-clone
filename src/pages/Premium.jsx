9ba5f65a5b953673b0113d9d9c6780047645c28e
90539 <yavuz.imer133@gmail.com>
2021-11-24 00:25:01 +0300

    cards

--- src/pages/Premium.jsx
+++ src/pages/Premium.jsx
@@ -54,16 +54,245 @@ function Premium() {
                 <h3 className="bottom-text">Telefonunda, hoparlöründe ve diğer cihazlarında sınırsız dinle.</h3>
                 <div className="bottom-grid">
                     <div className="bottom-card">
+                        <div className="general-card card-top">
+                            <div className="card-price-infor">1 Ay ücretsiz</div>
+                            <div className="card-payment-infor">Bir defalık ödeme yapılabilir</div>
+                            <h3>Bireysel</h3>
+                            <p>Teklif döneminden sonra ayda 17,99 TL</p>
+                            <p>1 hesap</p>
+                            <hr />
+                        </div>
+                        <div className="general-card card-middle">
+                            <ul>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        Müziklerini reklamsız dinle
+                                    </p>
+                                </li>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        Her yerde çal, hatta çevrimdışı dinle
+                                    </p>
+                                </li>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        İstediğin zaman çal
+                                    </p>
+                                </li>
+                            </ul>
+
+                        </div>
+                        <div className="general-card card-btn-field">
+                            <button>
+                                1 Ay Ücretsiz Kullanın
+                            </button>
+                        </div>
+                        <div className="general-card card-condutions">
+                            <a href="">DİĞER ÖDEME SEÇENEKLERİ</a>
+                            <p>
+                                <a href="">
+                                    Hüküm ve koşullar geçerlidir.
+                                </a>
+                                <span> Premium'u daha önce denemiş olan kullanıcılar 1 aylık ücretsiz tekliften yararlanamaz.</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="bottom-card">
+                        <div className="general-card card-top">
+                            <div className="card-price-infor">1 Ay ücretsiz</div>
+                            <h3>Duo</h3>
+                            <p>Teklif döneminden sonra ayda 23,99 TL</p>
+                            <p>2 hesap</p>
+                            <hr className="dif-hr" />
+                        </div>
+                        <div className="general-card card-middle">
+                            <ul>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        Aynı çatı altında yaşayan çiftler için 2 Premium hesabı
+                                    </p>
+                                </li>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        Duo Mix: İki kişi için bir çalma listesi, ikinizin tadını çıkaracağı müziklerle
+                                    </p>
+                                </li>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        Kesintisiz müzik dinle, çevrimdışı çal, istediğin zaman çal
+                                    </p>
+                                </li>
+                            </ul>
+
+                        </div>
+
+                        <div className="general-card card-btn-field">
+                            <button>
+                                1 Ay Ücretsiz Kullanın
+                            </button>
+                        </div>
+                        <div className="general-card card-condutions">
+                            <a href="">DİĞER ÖDEME SEÇENEKLERİ</a>
+                            <p>
+                                <a href="">
+                                    Hüküm ve koşullar geçerlidir.
+                                </a>
+                                <span> Premium'u daha önce denemiş olan kullanıcılar 1 aylık ücretsiz tekliften yararlanamaz.</span>
+                            </p>
+                        </div>
 
                     </div>
                     <div className="bottom-card">
+                        <div className="general-card card-top">
+                            <div className="card-price-infor">1 Ay ücretsiz</div>
+                            <h3>Aile</h3>
+                            <p>Teklif döneminden sonra ayda 29,99 TL</p>
+                            <p>6 hesaba kadar</p>
+                            <hr className="dif-hr" />
+                        </div>
+                        <div className="general-card card-middle">
+                            <ul>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        Aynı çatı altında yaşayan aile üyeleri için 6 ayrı Premium hesabı
+                                    </p>
+                                </li>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        Aile Mix'i: Ailen için bir çalma listesi, hepinizin tadını çıkaracağı müziklerle düzenli olarak güncellenir
+                                    </p>
+                                </li>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        Sansürsüz müziği engelle
+                                    </p>
+                                </li>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        Kesintisiz müzik dinle, çevrimdışı çal, istediğin zaman çal
+                                    </p>
+                                </li>
+                            </ul>
 
+                        </div>
+                        <div className="general-card card-btn-field">
+                            <button>
+                                1 Ay Ücretsiz Kullanın
+                            </button>
+                        </div>
+                        <div className="general-card card-condutions">
+                            <p>
+                                <a href="">
+                                    Hüküm ve koşullar geçerlidir.
+                                </a>
+                                <span>
+                                    Hüküm ve koşullar geçerlidir. Premium'u daha önce denemiş olan kullanıcılar 1 aylık ücretsiz tekliften yararlanamaz.
+                                </span>
+                            </p>
+                        </div>
                     </div>
                     <div className="bottom-card">
+                        <div className="general-card card-top">
+                            <div className="card-price-infor">1 Ay ücretsiz</div>
+                            <h3>Öğrenci</h3>
+                            <p>Teklif döneminden sonra ayda 8,99 TL</p>
+                            <p>1 hesap</p>
+                            <hr className="dif-hr" />
+                        </div>
+                        <div className="general-card card-middle">
+                            <ul>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        Yararlanabilecek üniversite öğrencilerine özel indirim
+                                    </p>
+                                </li>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        Müziklerini reklamsız dinle
+                                    </p>
+                                </li>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        Her yerde çal, hatta çevrimdışı dinle
+                                    </p>
+                                </li>
+                                <li>
+                                    <div> <svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" aria-label="" class="Svg-ytk21e-0 fJEWJR">
+                                        <polyline points="3.32 12.86 8.9 19.4 20.99 5.26" fill="none" stroke="#181818">
+                                        </polyline>
+                                    </svg></div>
+                                    <p>
+                                        İstediğin zaman çal
+                                    </p>
+                                </li>
+                            </ul>
 
-                    </div>
-                    <div className="bottom-card">
-
+                        </div>
+                        <div className="general-card card-btn-field">
+                            <button>
+                                1 Ay Ücretsiz Kullanın
+                            </button>
+                        </div>
+                        <div className="general-card card-condutions">
+                            <p>
+                                <a href="">
+                                    Hüküm ve koşullar geçerlidir.
+                                </a>
+                                <span> Teklif, yalnızca akredite olan yüksek öğrenim kurumlarındaki öğrenciler için geçerlidir. Premium'u daha önce denemiş olan kullanıcılar 1 aylık ücretsiz tekliften yararlanamaz. Spotify Öğrenci İndirimi Teklifi Hüküm ve koşullar geçerlidir.</span>
+                            </p>
+                        </div>
                     </div>
                 </div>
 
